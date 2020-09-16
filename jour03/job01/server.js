bf6e14827d18f24f1568e33d0a82ab6e3f273778
olivier-crozet <olivier.crozet@laplateforme.io>
2020-09-16 13:52:41 +0200

    job 1 fait mais pas assimilé

---
+++ jour03/job01/server.js
@@ -0,0 +1,20 @@
+
+
+const http = require('http');
+
+
+const server = http.createServer() 
+server.on('request', (request, response) => {
+  response.statusCode = 200;
+  response.setHeader('Content-Type', 'text/plain');
+  response.end('Hello World');
+});
+
+server.listen(3000)
+
+
+
+
+
+
+// console.log("hello world");
